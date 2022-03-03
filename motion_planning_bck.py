49545bb31875e8328e34aba2bb6420b503174adc
simk3 <34140324+johnadams2076@users.noreply.github.com>
2022-03-03 13:03:31 -0600

    Initial Commit.

--- motion_planning_bck.py
+++ motion_planning_bck.py
@@ -5,14 +5,11 @@ from enum import Enum, auto
 
 import numpy as np
 
-from planning_utils import a_star, heuristic, create_grid, get_lat_lon, prune_path, global_to_local, a_star_graph, \
-    create_grid_and_edges, closest_point, plot_grid
+from planning_utils import a_star, heuristic, create_grid
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
-import numpy.linalg as LA
-import networkx as nx
-
+from udacidrone.frame_utils import global_to_local
 
 
 class States(Enum):
@@ -27,7 +24,7 @@ class States(Enum):
 
 class MotionPlanning(Drone):
 
-    def __init__(self, connection, global_goal_position=np.array([40.0, 40.0, 3.0])):
+    def __init__(self, connection):
         super().__init__(connection)
 
         self.target_position = np.array([0.0, 0.0, 0.0])
@@ -35,13 +32,8 @@ class MotionPlanning(Drone):
         self.in_mission = True
         self.check_state = {}
 
-        self._latitude = 37.7849088
-        self._longitude = -122.4005945
-        self._altitude = 0
-
         # initial state
         self.flight_state = States.MANUAL
-        self.global_goal_position = global_goal_position
 
         # register all your callbacks here
         self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
@@ -127,77 +119,40 @@ class MotionPlanning(Drone):
 
         self.target_position[2] = TARGET_ALTITUDE
 
-        colliders_file = 'colliders.csv'
         # TODO: read lat0, lon0 from colliders into floating point values
-        lat0, lon0 = get_lat_lon(colliders_file)
-        print(f'Home lat : {lat0}, lon : {lon0}')
+        
         # TODO: set home position to (lon0, lat0, 0)
-        self.set_home_position(lon0, lat0, 0)
-
 
         # TODO: retrieve current global position
-        print('Current global position', self.global_position)
+
         # TODO: convert to current local position using global_to_local()
-        local_north, local_east, local_down = global_to_local(self.global_position, self.global_home)
-        print('global home {0}, global position {1}, local position {2}'.format(self.global_home, self.global_position,
+
+        print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
         
         # Define a grid for a particular altitude and safety margin around obstacles
-        #grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
-        print('Pre-create Grid and Edges')
-        # This is now the routine using Voronoi
-        grid, edges, north_offset, east_offset = create_grid_and_edges(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
-        print('Post-create Grid and Edges')
-        # TODO: create the graph with the weight of the edges
-        # set to the Euclidean distance between the points
-        G = nx.Graph()
-        for e in edges:
-            p1 = e[0]
-            p2 = e[1]
-            dist = LA.norm(np.array(p2) - np.array(p1))
-            G.add_edge(p1, p2, weight=dist)
-        print('Post-create Graph', G.number_of_nodes(), G.number_of_edges(), G.size())
-        #start_ne = (25, 100)
-        #goal_ne = (750., 300.)
-
-        #start_ne_g = closest_point(G, start_ne)
-        #goal_ne_g = closest_point(G, goal_ne)
+        grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
-        #grid_start = (-north_offset, -east_offset)
+        grid_start = (-north_offset, -east_offset)
         # TODO: convert start position to current position rather than map center
-        grid_start_north = int(np.ceil(local_north - north_offset))
-        grid_start_east = int(np.ceil(local_east - east_offset))
-        grid_start = (grid_start_north, grid_start_east)
-        graph_start = closest_point(G, grid_start)
-
+        
         # Set goal as some arbitrary position on the grid
-        #grid_goal = (-north_offset + 10, -east_offset + 10)
+        grid_goal = (-north_offset + 10, -east_offset + 10)
         # TODO: adapt to set goal as latitude / longitude position and convert
-        local_goal_north, local_goal_east, local_goal_down = global_to_local(self.global_goal_position,
-                                                                             self.global_home)
-        grid_goal_north = int(np.ceil(local_goal_north - north_offset))
-        grid_goal_east = int(np.ceil(local_goal_east - east_offset))
-        grid_goal = (grid_goal_north, grid_goal_east)
-        graph_goal = closest_point(G, grid_goal)
-
-        plot_grid(edges, grid, grid_start, grid_goal)
-
 
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
-
-        print('Local Start and Goal: ', graph_start, graph_goal)
-        path, path_cost = a_star_graph(G, heuristic, graph_start, graph_goal)
-        print('Post a-Star search')
+        print('Local Start and Goal: ', grid_start, grid_goal)
+        path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
-        pruned_path = prune_path(path)
+
         # Convert path to waypoints
-        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in pruned_path]
+        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
         # Set self.waypoints
         self.waypoints = waypoints
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
@@ -216,26 +171,14 @@ class MotionPlanning(Drone):
         self.stop_log()
 
 
-def main():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=5760, help='Port number')
     parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
-    #parser.add_argument('--global_goal_lon', type=str, default='-122.4165994', help="Goal position longitude")
-    #parser.add_argument('--global_goal_lat', type=str, default='37.8091669', help="Goal position latitude")
-    parser.add_argument('--global_goal_lon', type=str, default='-122.4005945', help="Goal position longitude")
-    parser.add_argument('--global_goal_lat', type=str, default='37.7850088', help="Goal position latitude")
-
-    parser.add_argument('--global_goal_alt', type=str, default='0', help="Goal position altitude")
     args = parser.parse_args()
-    print(args)
-    global_goal_position = np.fromstring(f'{args.global_goal_lon},{args.global_goal_lat},{args.global_goal_alt}',
-                                         dtype='Float64', sep=',')
 
-    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60000)
-    drone = MotionPlanning(conn, global_goal_position=global_goal_position)
+    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
+    drone = MotionPlanning(conn)
     time.sleep(1)
+
     drone.start()
-
-
-if __name__ == "__main__":
-    main()
--- planning_utils.py
+++ planning_utils.py
@@ -1,7 +1,13 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
+import utm
+from matplotlib import pyplot as plt
+from scipy.constants import point
+import numpy.linalg as LA
+from scipy.spatial import Voronoi, voronoi_plot_2d
 
+plt.rcParams['figure.figsize'] = 12, 12
 
 def create_grid(data, drone_altitude, safety_distance):
     """
@@ -143,4 +149,338 @@ def a_star(grid, h, start, goal):
 
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
+
+def get_data():
+# This is the same obstacle data from the previous lesson.
+    filename = 'colliders.csv'
+    data = np.loadtxt(filename, delimiter=',', dtype='float64', skiprows=2)
+
+    return data
+
+def get_lat_lon(filename):
+# This is the same obstacle data from the previous lesson.
+    data = np.loadtxt(filename, delimiter=',', dtype='str', max_rows=1)
+    data1 = data[0].split()
+    data2 = data[1].split()
+
+    return float(data1[1]), float(data2[1])
+
+
+def global_to_local(global_position, global_home):
+    # TODO: Get easting and northing of global_home
+    # TODO: Get easting and northing of global_position
+    # TODO: Create local_position from global and home positions
+    #local_position = numpy.array([0, 0, 0])
+    """
+    Convert a global position (lon, lat, up) to a local position (north, east, down) relative to the home position.
+    Returns:
+        numpy array of the local position [north, east, down]
+    """
+    (east_home, north_home, _, _) = utm.from_latlon(global_home[1], global_home[0])
+    (east, north, _, _) = utm.from_latlon(global_position[1], global_position[0])
+
+    local_position = np.array([north - north_home, east - east_home,  -(global_position[2] - global_home[2])])
+    return local_position
+
+
+def local_to_global(local_position, global_home):
+    # TODO: get easting, northing, zone letter and number of global_home
+    # TODO: get (lat, lon) from local_position and converted global_home
+    # TODO: Create global_position of (lat, lon, alt)
+
+    global_position = np.array([0, 0, 0])
+    """
+       Convert a local position (north, east, down) relative to the home position to a global position (lon, lat, up)
+       Returns:
+           numpy array of the global position [longitude, latitude, altitude]
+       """
+    (east_home, north_home, zone_number, zone_letter) = utm.from_latlon(global_home[1], global_home[0])
+    (lat, lon) = utm.to_latlon(east_home + local_position[1], north_home + local_position[0], zone_number, zone_letter)
+
+    global_position = np.array([lon, lat, -(local_position[2]-global_home[2])])
+    return global_position
+
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+def prune_path(path):
+    if path is not None:
+        pruned_path = [p for p in path]
+        # TODO: prune the path!
+        i = 0
+        print(len(pruned_path))
+        print(pruned_path)
+        print(pruned_path[0])
+        while i < len(pruned_path) - 2:
+            p1 = point(pruned_path[i])
+            p2 = point(pruned_path[i + 1])
+            p3 = point(pruned_path[i + 2])
+
+            # If the 3 points are in a line remove
+            # the 2nd point.
+            # The 3rd point now becomes and 2nd point
+            # and the check is redone with a new third point
+            # on the next iteration.
+            if collinearity_check(p1, p2, p3):
+                # Something subtle here but we can mutate
+                # `pruned_path` freely because the length
+                # of the list is checked on every iteration.
+                pruned_path.remove(pruned_path[i + 1])
+            else:
+                i += 1
+        else:
+            pruned_path = path
+
+    return pruned_path
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6):
+    m = np.concatenate((p1, p2, p3), 0)
+    det = np.linalg.det(m)
+    return abs(det) < epsilon
+
+###### THIS IS YOUR OLD GRID-BASED A* IMPLEMENTATION #######
+###### With a few minor modifications it can work with graphs! ####
+# TODO: modify A* to work with a graph
+def a_star_graph(graph, h, start, goal):
+
+    path = []
+    path_cost = 0
+    queue = PriorityQueue()
+    queue.put((0, start))
+    visited = set(start)
+
+    branch = {}
+    found = False
+
+    while not queue.empty():
+        item = queue.get()
+        current_node = item[1]
+        if current_node == start:
+            current_cost = 0.0
+        else:
+            current_cost = branch[current_node][0]
+
+        if current_node == goal:
+            print('Found a path.')
+            found = True
+            break
+        else:
+            for next_node in graph[current_node]:
+                cost = graph.edges[current_node, next_node]['weight']
+                branch_cost = current_cost + cost
+                queue_cost = branch_cost + h(next_node, goal)
+
+                if next_node not in visited:
+                    visited.add(next_node)
+                    branch[next_node] = (branch_cost, current_node)
+                    queue.put((queue_cost, next_node))
+
+    if found:
+        # retrace steps
+        n = goal
+        path_cost = branch[n][0]
+        path.append(goal)
+        while branch[n][1] != start:
+            path.append(branch[n][1])
+            n = branch[n][1]
+        path.append(branch[n][1])
+    else:
+        print('**********************')
+        print('Failed to find a path!')
+        print('**********************')
+    return path[::-1], path_cost
+
+def heuristic_graph(n1, n2):
+    # TODO: complete
+    return LA.norm(np.array(n2) - np.array(n1))
+
+class ActionPlus(Enum):
+    """
+    An action is represented by a 3 element tuple.
+
+    The first 2 values are the delta of the action relative
+    to the current grid position. The third and final value
+    is the cost of performing the action.
+    """
+    WEST = (0, -1, 1)
+    EAST = (0, 1, 1)
+    NORTH = (-1, 0, 1)
+    SOUTH = (1, 0, 1)
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
+
+    @property
+    def cost(self):
+        return self.value[2]
+
+    @property
+    def delta(self):
+        return (self.value[0], self.value[1])
+
+
+def valid_actions_plus(grid, current_node):
+    """
+       Returns a list of valid actions given a grid and current node.
+    """
+    valid_actions = list(ActionPlus)
+    n,m = grid.shape[0] - 1, grid.shape[1] - 1
+    x,y = current_node
+    # check if the node is off the grid or
+    # it's an obstacle
+
+    if x-1 < 0 or grid[x-1, y] == 1:
+        valid_actions.remove(ActionPlus.NORTH)
+    if x+1 > n or grid[x+1, y] ==1:
+        valid_actions.remove(ActionPlus.SOUTH)
+    if y-1 < 0 or grid[x, y - 1] == 1:
+        valid_actions.remove(ActionPlus.WEST)
+    if y+1 > m or grid[x, y+1] == 1:
+        valid_actions.remove(ActionPlus.EAST)
+
+    if (x -1 < 0 or y-1 < 0) or grid[x-1, y-1] == 1:
+        valid_actions.remove(ActionPlus.NORTH_WEST)
+    if (x - 1 < 0 or y + 1 > m) or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(ActionPlus.NORTH_EAST)
+    if (x + 1 > n or y - 1 < 0) or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(ActionPlus.SOUTH_WEST)
+    if (x + 1 > n or y + 1 > m) or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(ActionPlus.SOUTH_EAST)
+
+    return valid_actions
+
+
+# If you want to use the prebuilt bresenham method
+# Import the Bresenham package
+from bresenham import bresenham
+# Here you'll modify the `create_grid()` method from a previous exercise
+# In this new function you'll record obstacle centres and
+# create a Voronoi graph around those points
+def create_grid_and_edges(data, drone_altitude, safety_distance):
+    """
+    Returns a grid representation of a 2D configuration space
+    along with Voronoi graph edges given obstacle data and the
+    drone's altitude.
+    """
+
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
+
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
+
+    # given the minimum and maximum coordinates we can
+    # calculate the size of the grid.
+    north_size = int(np.ceil((north_max - north_min)))
+    east_size = int(np.ceil((east_max - east_min)))
+
+    # Initialize an empty grid
+    grid = np.zeros((north_size, east_size))
+    # Center offset for grid
+    north_min_center = np.min(data[:, 0])
+    east_min_center = np.min(data[:, 1])
+
+    # Define a list to hold Voronoi points
+    points = []
+    # Populate the grid with obstacles
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+
+        if alt + d_alt + safety_distance > drone_altitude:
+            obstacle = [
+                int(north - d_north - safety_distance - north_min_center),
+                int(north + d_north + safety_distance - north_min_center),
+                int(east - d_east - safety_distance - east_min_center),
+                int(east + d_east + safety_distance - east_min_center),
+            ]
+            grid[obstacle[0]:obstacle[1] + 1, obstacle[2]:obstacle[3] + 1] = 1
+
+            # add center of obstacles to points list
+            points.append([north - north_min, east - east_min])
+
+    # TODO: create a voronoi graph based on
+    # location of obstacle centres
+    graph = Voronoi(points)
+
+    # TODO: check each edge from graph.ridge_vertices for collision
+    edges = []
+    for v in graph.ridge_vertices:
+        p1 = graph.vertices[v[0]]
+        p2 = graph.vertices[v[1]]
+        cells = list(bresenham(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1])))
+        hit = False
+
+        for c in cells:
+            # First check if we're off the map
+            if np.amin(c) < 0 or c[0] >= grid.shape[0] or c[1] >= grid.shape[1]:
+                hit = True
+                break
+            # Next check if we're in collision
+            if grid[c[0], c[1]] == 1:
+                hit = True
+                break
+
+        # If the edge does not hit on obstacle
+        # add it to the list
+        if not hit:
+            # array to tuple for future graph creation step)
+            p1 = (p1[0], p1[1])
+            p2 = (p2[0], p2[1])
+            edges.append((p1, p2))
+
+    return grid, edges, int(north_min), int(east_min)
+
+def closest_point(graph, current_point):
+    """
+    Compute the closest point in the `graph`
+    to the `current_point`.
+    """
+    closest_point  = None
+    #closest_point = graph.nodes[0]
+    dist = 100000
+    for p in graph.nodes:
+        d = LA.norm(np.array(p) - np.array(current_point))
+        if d < dist:
+            closest_point = p
+            dist = d
+    return closest_point
+
+
+def plot_grid(edges, grid, start_ne, goal_ne):
+    print(len(edges))
+    # equivalent to
+    # plt.imshow(np.flip(grid, 0))
+    plt.imshow(grid, origin='lower', cmap='Greys')
+    for e in edges:
+        p1 = e[0]
+        p2 = e[1]
+        plt.plot([p1[1], p2[1]], [p1[0], p2[0]], 'b-')
+    plt.plot(start_ne[1], start_ne[0], 'rx')
+    plt.plot(goal_ne[1], goal_ne[0], 'rx')
+    plt.xlabel('EAST')
+    plt.ylabel('NORTH')
+    plt.show()
+
+
+def plot_path(path, grid, edges, start_ne, goal_ne, start_ne_g, goal_ne_g):
+    plt.imshow(grid, origin='lower', cmap='Greys')
+    for e in edges:
+        p1 = e[0]
+        p2 = e[1]
+        plt.plot([p1[1], p2[1]], [p1[0], p2[0]], 'b-')
+    plt.plot([start_ne[1], start_ne_g[1]], [start_ne[0], start_ne_g[0]], 'r-')
+    for i in range(len(path) - 1):
+        p1 = path[i]
+        p2 = path[i + 1]
+        plt.plot([p1[1], p2[1]], [p1[0], p2[0]], 'r-')
+    plt.plot([goal_ne[1], goal_ne_g[1]], [goal_ne[0], goal_ne_g[0]], 'r-')
+    plt.plot(start_ne[1], start_ne[0], 'gx')
+    plt.plot(goal_ne[1], goal_ne[0], 'gx')
+    plt.xlabel('EAST', fontsize=20)
+    plt.ylabel('NORTH', fontsize=20)
+    plt.show()
+
 
